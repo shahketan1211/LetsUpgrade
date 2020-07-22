509b497f4f258f25be0346d9080a5e0e84f91c74
KETAN SHAH <shahketan1211@gmail.com>
2020-07-23 00:12:05 +0530

    Create Day2.py

---
+++ Assignment_2/Day2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[6]:
+
+
+#operators
+#Arithmatic operator
+a=10
+b=20
+c=a+b
+c=a*b
+print("c=",c)
+c=b/a
+print("c=",c)
+c=a**b
+print("c=",c)
+
+print("c=",c)
+
+
+# In[7]:
+
+
+#Assignment Operator
+a=10
+b=a
+print("b=",b)
+a+=b
+print("a=",a)
+a*=b
+print("a=",a)
+a/=b
+print("a=",a)
+a**=b
+print("a=",a)
+
+
+# In[8]:
+
+
+#Logical Operator
+a=1
+b=20
+a>20
+
+
+# In[ ]:
+
+
+
+
