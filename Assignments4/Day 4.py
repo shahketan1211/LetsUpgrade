30eaaae86c4e5a904d17f8b0c4d0b6ba4b10d45e
KETAN SHAH <shahketan1211@gmail.com>
2020-07-23 21:26:17 +0530

    Create Day 4.py

---
+++ Assignments4/Day 4.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Day 4 Assignment
+get_ipython().set_next_input('find all occurence of substring in the given string');get_ipython().run_line_magic('pinfo', 'string')
+
+
+# In[3]:
+
+
+str1="what we think we become,what it is"
+str1.find("we")
+
+
+# In[4]:
+
+
+str1.rfind("we")
+
+
+# In[5]:
+
+
+str1.count("we")
+
+
+# In[6]:
+
+
+str1.find("what")
+
+
+# In[7]:
+
+
+str1.rfind("what")
+
+
+# In[8]:
+
+
+str1.count("what")
+
+
+# In[9]:
+
+
+str1.index("we")
+
+
+# In[10]:
+
+
+str1="what we think we become,what it is"
+str1.find("we")
+str1.rfind("we")
+str1.find("what")
+str1.rfind("what")
+print(str1)
+
+
+# In[11]:
+
+
+str1="python"
+str2="programmer"
+str3="letsupgrade"
+str4="batchsix"
+str1.upper()
+
+
+# In[12]:
+
+
+str5=str2.upper()
+print(str5)
+
+
+# In[13]:
+
+
+str3.upper()
+
+
+# In[14]:
+
+
+str4.upper()
+
+
+# In[15]:
+
+
+str1.islower()
+
+
+# In[16]:
+
+
+str1="PYTHON"
+str2="PROGRAMMER"
+str3="LETSUPGRADE"
+str4="BATCHSIX"
+str1.lower()
+
+
+# In[17]:
+
+
+str2.lower()
+
+
+# In[18]:
+
+
+str3.lower()
+
+
+# In[19]:
+
+
+str4.lower()
+
+
+# In[20]:
+
+
+str1.isupper()
+
